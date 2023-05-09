8a6dd7187fd4831e520ca8df7f24913a653c0934
Nick Elobsky <echonok@gmail.com>
2023-05-09 19:34:36 +0200

    http tests

--- src/app/courses/services/courses-service.spec.ts
+++ src/app/courses/services/courses-service.spec.ts
@@ -2,6 +2,7 @@ import { CoursesService } from './courses.service';
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { COURSES } from '../../../../server/db-data';
+import { Course } from '../model/course';
 
 describe('CoursesService', () => {
   let coursesService: CoursesService;
@@ -34,4 +35,30 @@ describe('CoursesService', () => {
     expect(req.request.method).toEqual('GET');
     req.flush({ payload: Object.values(COURSES) });
   });
+
+  it('should find course by id', () => {
+    coursesService.findCourseById(12).subscribe((course) => {
+      expect(course).withContext('No course').toBeTruthy();
+      expect(course.id).withContext('incorrect number of courses').toBe(12)
+    });
+    const req = httpTestingController.expectOne('/api/courses/12');
+    expect(req.request.method).toEqual('GET');
+    req.flush(COURSES[12]);
+  });
+
+  it('should save course by id', () => {
+    const changes: Partial<Course> = { titles: { description: 'test' } };
+    coursesService.saveCourse(12, changes).subscribe((course) => {
+      expect(course).withContext('No course').toBeTruthy();
+      expect(course.id).withContext('incorrect id').toBe(12);
+    });
+    const req = httpTestingController.expectOne('/api/courses/12');
+    expect(req.request.method).toEqual('PUT');
+    expect(req.request.body.titles.description).toEqual(changes.titles.description);
+    req.flush({ ...COURSES[12], ...changes });
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  })
 });
